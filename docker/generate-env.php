1342b3fdd496804f2b24a02f8c588655442fc386
Veliona092 <kyle4k092@gmail.com>
2026-08-31 23:50:48 +0800

    Auto-clean quotation marks from environment variables and Cloudflare credentials

--- docker/generate-env.php
+++ docker/generate-env.php
@@ -11,10 +11,17 @@ foreach ($envVars as $key => $value) {
     }
 
     if (preg_match('/^(APP_|DB_|DATABASE_|MYSQL|SESSION_|CACHE_|MAIL_|QUEUE_|LOG_|CLOUDFLARE_|PORT)/', $key)) {
+        // Strip any surrounding quotes the user may have pasted in Railway UI
+        $cleanValue = trim($value);
+        if ((str_starts_with($cleanValue, '"') && str_ends_with($cleanValue, '"')) ||
+            (str_starts_with($cleanValue, "'") && str_ends_with($cleanValue, "'"))) {
+            $cleanValue = substr($cleanValue, 1, -1);
+        }
+
         // Escape backslashes and double quotes
-        $escaped = str_replace(['\\', '"'], ['\\\\', '\\"'], $value);
+        $escaped = str_replace(['\\', '"'], ['\\\\', '\\"'], $cleanValue);
         $lines[] = "{$key}=\"{$escaped}\"";
     }
 }
 
-file_put_contents('/var/www/html/.env', implode("\n", $lines) . "\n");
+file_put_contents('/var/www/html/.env', implode("\n", $lines)."\n");
