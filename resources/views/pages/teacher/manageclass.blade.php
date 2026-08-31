4f5178e5beb82285e312e407d72407e4959b1076
Veliona092 <kyle4k092@gmail.com>
2026-08-31 21:33:19 +0800

    Fix missing script tag and force maximum z-index on rv-drawer

--- resources/views/pages/teacher/manageclass.blade.php
+++ resources/views/pages/teacher/manageclass.blade.php
@@ -1916,8 +1916,7 @@ window.closeCreateDialog = function() {
 
 <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
 
-
-
+<script>
 window.safeOpenDialog = function(dialogId) {
     const el = document.getElementById(dialogId);
     if (!el) return;
